5bc8fec59ee582e4eca6b20cec6f1871cbd9aa79
Abhishek Mishra <abhishekmishra3@gmail.com>
2020-04-12 18:26:33 +0530

    reading yaml config as a deserialized class

--- S2CServer/S2CServer/Program.cs
+++ S2CServer/S2CServer/Program.cs
@@ -7,49 +7,10 @@ namespace S2CServer
         static void Main(string[] args)
         {
 
-            new SimulationController();
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-
             Console.WriteLine("Created new space to clean -> ");
-            var sp = new Space(10, 10, 0.0, 0.3);
-            int agent = 0;
-            int r = 0, c = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                try
-                {
-                    agent = sp.initAgent(i, i);
-                    r = c = i;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    continue;
-                }
-                break;
-            }
-            sp.printSpace();
-            if (agent > 0)
-            {
-                try
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        sp.moveAgent(agent, r, c + i);
-                        bool cleaned = sp.clean(agent, r, c + i);
-                        if (cleaned)
-                        {
-                            Console.WriteLine("Cleaned [" + r + ", " + (c + i) + "]");
-                        }
-                    }
-                }catch(Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            }
 
-            Console.WriteLine();
-            sp.printSpace();
+            var sc = new SimulationController();
         }
     }
 }
--- S2CServer/S2CServer/SimulationController.cs
+++ S2CServer/S2CServer/SimulationController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace S2CServer
 {
@@ -16,42 +19,92 @@ namespace S2CServer
     {
         public SimulationController()
         {
+            LoadConfig();
+
+            var sp = new Space(10, 10, 0.0, 0.3);
+            int agent = 0;
+            int r = 0, c = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    agent = sp.initAgent(i, i);
+                    r = c = i;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                break;
+            }
+            sp.printSpace();
+            if (agent > 0)
+            {
+                try
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        sp.moveAgent(agent, r, c + i);
+                        bool cleaned = sp.clean(agent, r, c + i);
+                        if (cleaned)
+                        {
+                            Console.WriteLine("Cleaned [" + r + ", " + (c + i) + "]");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine();
+            sp.printSpace();
+
+        }
+
+        private static void LoadConfig()
+        {
             // Setup the input
             var input = new StringReader(Document);
 
-            // Load the stream
-            var yaml = new YamlStream();
-            yaml.Load(input);
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
 
-            // Examine the stream
-            var mapping =
-                (YamlMappingNode)yaml.Documents[0].RootNode;
-
-            foreach (var entry in mapping.Children)
-            {
-                Console.WriteLine(((YamlScalarNode)entry.Key).Value);
-            }
-
-            // List all the items
-            var agents = (YamlSequenceNode)mapping.Children[new YamlScalarNode("agents")];
-            foreach (YamlMappingNode agent in agents)
-            {
-                Console.WriteLine(
-                    "{0}",
-                    agent.Children[new YamlScalarNode("type")]);
-            }
+            var simConfig = deserializer.Deserialize<SimConfig>(input);
+            Console.WriteLine(simConfig.Space.Rows);
         }
-
 
         private const string Document = @"---
             space:
-                nrow: 10
-                ncol: 10
-                dirt_prob: 0.3
-                wall_prob: 0.0
+                rows: 10
+                columns: 10
+                dirtProbability: 0.3
+                wallProbability: 0.0
 
             agents:
                 - type: simple
 ...";
+    }
+
+    public class SimConfig
+    {
+        public SpaceConfig Space;
+        public List<AgentConfig> Agents;
+    }
+
+    public class SpaceConfig
+    {
+        public int Rows;
+        public int Columns;
+        public double DirtProbability;
+        public double WallProbability; 
+    }
+
+    public class AgentConfig
+    {
+        public string Type;
     }
 }
