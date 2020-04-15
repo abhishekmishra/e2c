cfc265fd6d04969c2baba0bc54e93b7ab3c6b097
Abhishek Mishra <abhishekmishra3@gmail.com>
2020-04-15 20:25:29 +0530

    agent factory incomplete

---
+++ S2CServer/S2CServer/AgentFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace S2CCore
+{
+    public class AgentFactory
+    {
+        private static Assembly _CurrentAssembly = typeof(AgentFactory).Assembly;
+        private static readonly Dictionary<string, Type> _AgentTypes
+            = new Dictionary<string, Type>()
+            {
+                { "simple", _CurrentAssembly.GetType("S2Core.SimpleCleaningAgent") } 
+            };
+        public AgentFactory()
+        {
+        }
+    }
+}
