7931f3ab2001b70aa5f972ace3c3ad5bbe77dd1a
Abhishek Mishra <abhishekmishra3@gmail.com>
2020-04-16 13:56:20 +0530

    added agent score table

--- S2CServer/S2CServices/wwwroot/js/index.js
+++ S2CServer/S2CServices/wwwroot/js/index.js
@@ -48,6 +48,39 @@ function abort_sim() {
         });
 }
 
+function tabulateScores(data, columns) {
+    $("#score_table").html("");
+    var table = d3.select('#score_table').append('table')
+    var thead = table.append('thead')
+    var tbody = table.append('tbody');
+
+    // append the header row
+    thead.append('tr')
+        .selectAll('th')
+        .data(columns).enter()
+        .append('th')
+        .text(function (column) { return column; });
+
+    // create a row for each object in the data
+    var rows = tbody.selectAll('tr')
+        .data(data)
+        .enter()
+        .append('tr');
+
+    // create a cell in each row for each column
+    var cells = rows.selectAll('td')
+        .data(function (row) {
+            return columns.map(function (column) {
+                return { column: column, value: row[column] };
+            });
+        })
+        .enter()
+        .append('td')
+        .text(function (d) { return d.value; });
+
+    return table;
+}
+
 
 function new_sim() {
     cmdsCount = 0;
@@ -103,6 +136,7 @@ function fetchRound(id, roundNum) {
             var status = JSON.parse(statusText);
             //console.log(status);
             d3.select("#round_num").text(status.roundNum);
+            tabulateScores(status.agentStatistics, ["agentId", "clean", "moves", "efficiency", "errorRate"]);
             d3.select("#sim").html("");
 
             var w = 400, h = 400;
